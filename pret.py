041b16903c98352f427e49ba6a6bdea3f8a89c8f
Philippe Legeard <phlegeard@yahoo.fr>
2026-02-10 06:48:45 -0800

    2nd commit

--- pret.py
+++ pret.py
@@ -1,11 +1,26 @@
 import matplotlib.pyplot as plt
 
 # constants
-P = 1500000
-Ta = 6/100
+_ONE_MILLION_ = 1000000
+P = 1.5*_ONE_MILLION_
+T = 5.25
+Ta = T/100
 Tm = Ta/12
 Y = 30
 Ym = Y*12
+
+# income and tax brackets
+W2_1 = 300000
+W2_2 = 540000
+W2 = W2_1 + W2_2
+Non_itemized_federal_standard_deduction = 29200
+Non_itemized_state_standard_deduction = 30000
+
+Taxable_income = W2
+
+print("Taxable_income: ",Taxable_income)
+Federal_tax_brackets = [[10,0],[12,23850],[22,96950],[24,206700],[32,394600],[35,501050],[37,751600]]
+State_tax_brackets = [[1,0],[2,20199],[4,47885],[6,75577],[8,104911],[9.3,132591] ,[10.3,677279],[11.3,812729],[12.3,1354557]]
 
 #summary
 print("montant emprunte: ", P)
@@ -20,34 +35,12 @@ for i in range (Ym):
     Denominator += (1+Tm)**i
   
 # remboursement mensuel sans assurance
+print (" ------------ Remboursemnt mensuel  ------------ ")
 rm = Numerator/Denominator
 print ("remboursement mensuel: ", rm)
 
-## Interets mensuels
-#months = []
-#Interets = []
-#reste_a_rembourser = P
-#month = 1
-#while (reste_a_rembourser > 0):
-#    months.append(month)
-#    Interets.append(reste_a_rembourser*Tm)
-#    reste_a_rembourser = reste_a_rembourser - rm + (reste_a_rembourser*Tm)
-#    month += 1
-#
-## Create plot
-#plt.plot(months, Interets, marker='o', color='blue', linestyle='--', label='Interets mensuels')
-#
-## Add labels and title
-#plt.xlabel('Mois')
-#plt.ylabel('Interets')
-#plt.title('Interets')
-#plt.legend()
-#
-## Show plot
-#plt.show()
-
-
 # Interets annuels
+print (" ------------ Interets annuels ------------ ")
 years = []
 Interets = []
 reste_a_rembourser = P
@@ -60,28 +53,77 @@ while (reste_a_rembourser > 0):
     reste_a_rembourser = reste_a_rembourser - rm + (reste_a_rembourser*Tm)
     if month%12==0:
         years.append(year)
-        print ("Interets annee:", year, " : ",Interets_annee_en_cours)
+        print ("Interets annee:", year, " : ", int(Interets_annee_en_cours))
         Interets.append(Interets_annee_en_cours)
         if year == 1:
-            Interets_year_1 = Interets_annee_en_cours
+            Interets_year_1 = int(Interets_annee_en_cours)
         Interets_annee_en_cours = 0
         year += 1
     month+=1
     
 
 # Tax deduction
-W2_1 = 300000
-W2_2 = 540000
-W2 = W2_1 + W2_2
-Taxable_income = W2 - 29200
-print("Taxable_income: ",Taxable_income)
-Federal_tax_brackets = [[10,0],[12,23850],[22,96950],[24,206700],[32,394600],[35,501050],[37,751600]]
-State_tax_brackets = [[1,0],[2,20199],[4,47885],[6,75577],[8,104911],[9.3,132591] ,[10.3,677279],[11.3,812729],[12.3,1354557]]
-
+print (" ------------ Tax deduction  ------------ ")
 print("Interets_year_1: ", Interets_year_1)
+    
+# Find the brackets that correspond to the tax income
+def find_what_deductible_to_what_bracket(w_d_t_w_b, tax_brackets):
+    deductible_of_this_bracket=0
+    total_deductible_for_all_brackets=0
+    for i in range (len(tax_brackets)-1, -1,-1): # if len of list brackets is , i goes from 6 to 0
+        if total_deductible_for_all_brackets == 0:
+            # total_deductible_for_all_brackets == 0 means that we have not found yet the highest bracket
+            # that corresponds to the taxable income
+            if Taxable_income > tax_brackets[i][1]:
+                if Interets_year_1 < Taxable_income - tax_brackets[i][1]:
+                    deductible_of_this_bracket = Interets_year_1
+                    total_deductible_for_all_brackets = Interets_year_1
+                    w_d_t_w_b.append([i,Interets_year_1])
+                else:
+                    deductible_of_this_bracket = Taxable_income - tax_brackets[i][1]
+                    total_deductible_for_all_brackets = deductible_of_this_bracket
+                    w_d_t_w_b.append([i,deductible_of_this_bracket])
+        elif total_deductible_for_all_brackets<Interets_year_1:
+            # if taxable income is in this bracket 
+            # let see how much of interest of the year correspond to
+            if tax_brackets[i+1][1]-tax_brackets[i][1] > Interets_year_1-total_deductible_for_all_brackets:
+                deductible_of_this_bracket = Interets_year_1-total_deductible_for_all_brackets
+                total_deductible_for_all_brackets = Interets_year_1
+                w_d_t_w_b.append([i,deductible_of_this_bracket])
+            else:
+                deductible_of_this_bracket = tax_brackets[i+1][1]-tax_brackets[i][1]
+                total_deductible_for_all_brackets += deductible_of_this_bracket
+                w_d_t_w_b.append([i,deductible_of_this_bracket])
+
+print(" --- Federal ---")
+print("In case of itemization, here is what can be deducted from federal tax income")
+what_deductible_to_what_bracket = []
+find_what_deductible_to_what_bracket(what_deductible_to_what_bracket, Federal_tax_brackets)
+print(what_deductible_to_what_bracket)
+saving = 0
+for i in range(len(what_deductible_to_what_bracket)):
+    tax_bracket_index = what_deductible_to_what_bracket[i][0]
+    saving += Federal_tax_brackets[tax_bracket_index][0]*what_deductible_to_what_bracket[i][1]/100
+print("saving: ", int(saving))
+
+print(" --- State ---")
+print("In case of itemization, here is what can be deducted from state tax income")
+what_deductible_to_what_bracket = []
+find_what_deductible_to_what_bracket(what_deductible_to_what_bracket, State_tax_brackets)
+print(what_deductible_to_what_bracket)
+saving = 0
+for i in range(len(what_deductible_to_what_bracket)):
+    tax_bracket_index = what_deductible_to_what_bracket[i][0]
+    saving += State_tax_brackets[tax_bracket_index][0]*what_deductible_to_what_bracket[i][1]/100
+print("saving: ", int(saving))
+
+
+
+print(" --- verify ---")
 Federal_tax_discount_year_1 = (Taxable_income - 751600)*0.37 + (Interets_year_1 - (Taxable_income - 751600))*0.35
 State_tax_discount_year_1 = Interets_year_1*0.103
 Total_tax_discount_year_1 =  Federal_tax_discount_year_1 + State_tax_discount_year_1
+
 print("Federal_tax_discount_year_1: ", Federal_tax_discount_year_1)
 print("State_tax_discount_year_1: ", State_tax_discount_year_1)
 print("Total_tax_discount_year_1: ", Total_tax_discount_year_1)
